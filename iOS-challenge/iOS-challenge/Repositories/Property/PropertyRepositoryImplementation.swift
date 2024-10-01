e5f47ed6a963ac8aa6122958428f4f84663e1196
Silvia España Gil <81619759+silviaespanagil@users.noreply.github.com>
2024-10-01 15:03:31 +0200

    feat: Create PropertyRepositoryImplementation for data handling

---
+++ iOS-challenge/iOS-challenge/Repositories/Property/PropertyRepositoryImplementation.swift
@@ -0,0 +1,24 @@
+//
+//  PropertyRepositoryImplementation.swift
+//  iOS-challenge
+//
+//  Created by Silvia España Gil on 1/10/24.
+//
+
+import Combine
+
+class PropertyRepositoryImplementation: PropertyRepository {
+    
+    private let dataSource: PropertyDataSource
+    private var cancellables: Set<AnyCancellable> = []
+    
+    init(dataSource: PropertyDataSource) {
+        
+        self.dataSource = dataSource
+    }
+    
+    func getPropertyList() -> AnyPublisher<[Property], Error> {
+        
+        dataSource.getPropertyList()
+    }
+}
