b48394a19945c141cfb7e3bcd7b88fb232bba96a
Silvia España Gil <81619759+silviaespanagil@users.noreply.github.com>
2024-10-01 13:49:27 +0200

    Project created

---
+++ iOS-challenge/iOS-challenge/iOS_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_challengeApp.swift
+//  iOS-challenge
+//
+//  Created by Silvia España Gil on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
