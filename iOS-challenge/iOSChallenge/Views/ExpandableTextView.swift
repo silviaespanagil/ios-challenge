68ae4ef4a8f2ac51d6d8da80d3990a3965765df4
Silvia España Gil <81619759+silviaespanagil@users.noreply.github.com>
2024-10-02 17:47:03 +0200

    feat: Create ExpandableText Component

---
+++ iOS-challenge/iOSChallenge/Views/ExpandableTextView.swift
@@ -0,0 +1,36 @@
+//
+//  ExpandableTextView.swift
+//  iOSChallenge
+//
+//  Created by Silvia España Gil on 2/10/24.
+//
+
+import SwiftUI
+
+struct ExpandableTextView: View {
+    
+    let text: String
+    
+    @State private var isExpanded = false
+    
+    var body: some View {
+        
+        VStack(alignment: .leading, spacing: .zero) {
+            
+            Text(text)
+                .lineLimit(isExpanded ? nil : 9)
+                .lineSpacing(6)
+                .padding(.bottom, 6)
+            
+            Button(action: {
+                withAnimation {
+                    isExpanded.toggle()
+                }
+            }) {
+                Text(isExpanded ? "Ver menos" : "Ver más")
+                    .font(.body)
+                    .foregroundColor(.blue)
+            }
+        }.padding(.vertical, 8)
+    }
+}
