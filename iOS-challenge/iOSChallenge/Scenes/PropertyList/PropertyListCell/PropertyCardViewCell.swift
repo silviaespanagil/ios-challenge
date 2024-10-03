08a1080b9058c88790472aae76963723a66b19f9
Silvia España Gil <81619759+silviaespanagil@users.noreply.github.com>
2024-10-03 13:04:45 +0200

    feat: Create new PropertyCardViewCell file and xib - Set card appearance with corners - Clip image to fit card corners

---
+++ iOS-challenge/iOSChallenge/Scenes/PropertyList/PropertyListCell/PropertyCardViewCell.swift
@@ -0,0 +1,63 @@
+//
+//  PropertyCardViewCell.swift
+//  iOSChallenge
+//
+//  Created by Silvia España Gil on 3/10/24.
+//
+
+import UIKit
+
+class PropertyCardViewCell: UITableViewCell {
+    
+    @IBOutlet weak var contentViewCell: UIView!
+    @IBOutlet weak var propertyImageView: UIImageView!
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var propertyTitle: UILabel!
+    @IBOutlet weak var propertyOperationType: UILabel!
+    @IBOutlet weak var propertyPrice: UILabel!
+    @IBOutlet weak var propertySize: UILabel!
+    @IBOutlet weak var propertyRooms: UILabel!
+    @IBOutlet weak var propertyBathrooms: UILabel!
+    
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        
+        setupCellAppearance()
+    }
+    
+    private func setupCellAppearance() {
+        
+        contentViewCell.clipsToBounds = false
+        contentViewCell.layer.cornerRadius = 15
+        contentViewCell.backgroundColor = UIColor.white
+        
+        contentViewCell.layer.borderWidth = 1
+        contentViewCell.layer.borderColor = UIColor.lightGray.cgColor
+        
+        propertyImageView.clipsToBounds = true
+        propertyImageView.layer.cornerRadius = 15
+        propertyImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            propertyImageView.topAnchor.constraint(equalTo: contentViewCell.topAnchor),
+            propertyImageView.leadingAnchor.constraint(equalTo: contentViewCell.leadingAnchor),
+            propertyImageView.trailingAnchor.constraint(equalTo: contentViewCell.trailingAnchor)
+        ])
+        
+        selectionStyle = .none
+    }
+    
+    internal func updateCardContent(with property: Property) {
+        
+        if let thumbnailURL = URL(string: property.thumbnail) {
+            propertyImageView.loadImage(from: thumbnailURL)
+        }
+        
+        propertyTitle.text = property.address.capitalizedFirstLetter
+        propertyOperationType.text = "\(property.propertyType.capitalizedFirstLetter) \(property.operation) - \(property.neighborhood), \(property.municipality)"
+        propertyPrice.text = "\(property.priceInfo.price.amount.formattedDouble) \(property.priceInfo.price.currencySuffix)"
+        propertySize.text = "\(property.size.formattedDouble) m²"
+        propertyRooms.text = "\(property.rooms) hab."
+        propertyBathrooms.text = "\(property.bathrooms) baños"
+    }
+}
